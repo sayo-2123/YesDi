c0c69a00a088bb4638ede3c430a2a309e6f43809
sayori33 <classa1990@gmail.com>
2023-12-16 02:28:36 +0900

    入力値の3桁数字チェック

--- src/main/java/com/example/demo/ShainForm.java
+++ src/main/java/com/example/demo/ShainForm.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 
 public class ShainForm {
 
 	@NotEmpty(message = "社員Noを入力してくださいね")
+	@Pattern(regexp ="^[0-9]{3}$", message="半角数字、3ケタでお願いします")
 	private String number;
 
 	public String getNumber() {
